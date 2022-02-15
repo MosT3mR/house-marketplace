570bd913b0cbdbaf25fcf477209c04596e6b634a
Othman Alomair <most3mr@live.com>
2022-02-15 18:09:35 +0300

    day 9 feeling good, writing late

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import SingUp from "./pages/SignUp"
 import SingIn from "./pages/SignIn"
 import Category from "./pages/Category"
 import CreateListing from "./pages/CreateListing"
+import Listing from "./pages/Listing"
+import Contact from "./pages/Contact"
 
 function App() {
   return (
@@ -33,6 +35,8 @@ function App() {
         <Route path="/sign-up" element={<SingUp />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/create-listing" element={<CreateListing />} />
+        <Route path="/category/:categoryName/:listingId" element={<Listing />} />
+        <Route path="/contact/:landlordId" element={<Contact />} />
       </Routes>
       <Navbar />
     </BrowserRouter>
